d3fac59fdc2606a300366fc075a6257341d23885
nya <YukiNakahiraCreative@gmail.com>
2024-12-05 09:35:43 +0900

    ignoreを有効化するために一回移動

--- DxLibGame_Winter/Calculation.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// ’è”’u‚¢‚Ä‚ ‚é‚¾‚¯
-namespace Calculation
-{
-	constexpr float kPi = 3.141592653589f;
-	constexpr float kRadToDeg = 180 / kPi;
-	constexpr float kDegToRad = kPi / 180;
-}
