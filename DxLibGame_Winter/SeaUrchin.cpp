a061f7be83047348a50501b092f35dba1d17dfb2
Yuki_School <YukiNakahiraCreative@gmail.com>
2025-02-05 12:53:12 +0900

    音量のスクリプト消した
    あとウニのバグ取り中

--- DxLibGame_Winter/SeaUrchin.cpp
+++ DxLibGame_Winter/SeaUrchin.cpp
@@ -3,18 +3,27 @@
 #include "Image.h"
 #include "Player.h"
 #include "SeaUrchin.h"
+#include "Physics.h"
 #include <string>
 
 namespace
 {
 	const std::string kImageFile = "uni_murasaki_close.png";
-	constexpr int kRotateSpeed = 1;
+	constexpr float kRotateSpeed = 0.5f;
+	constexpr float kColRadius = 30;
+	// 押されてもテコでも動かない
+	constexpr float kWeight = 9999;
+	constexpr float kVolume = 1;
 }
 
 SeaUrchin::SeaUrchin(ObjectsController& cont, const Vector2& initPos, Camera& camera, Player& player, const Vector2Int& mapPos) :
 	Enemy(cont, player, camera, initPos, mapPos)
 {
+	m_col = std::make_shared<CircleCollider>(m_pos, kColRadius);
 	m_image = std::make_shared<Image>(kImageFile);
+	m_image->SetExRate(0.2f);
+
+	m_physics = std::make_shared<Physics>(kWeight, kVolume);
 }
 
 void SeaUrchin::Update()
@@ -25,7 +34,7 @@ void SeaUrchin::Update()
 	{
 		// プレイヤーにダメージ!!!
 		m_playerRef.OnDamage();
-		m_playerRef.AddForce(collision.overlap);
+		m_playerRef.AddForce(-collision.overlap);
 	}
 
 	// 画像を回転
